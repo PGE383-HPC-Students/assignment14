1eab23fb50fb96c557dcc7ee7dce41d598b75872
John T. Foster <john.foster@utexas.edu>
2020-04-16 14:35:37 -0500

    Replace/Update test.py

--- test.py
+++ test.py
@@ -1,47 +1,16 @@
 #!/usr/bin/env python
 
+import os.path
 import unittest
-
-import nbconvert
-
-import numpy as np
-
-
-with open("assignment14.ipynb") as f:
-    exporter = nbconvert.PythonExporter()
-    python_file, _ = exporter.from_file(f)
-
-with open("assignment14.py", "w") as f:
-    f.write(python_file)
-
-from assignment14 import LU
-
 
 class TestSolution(unittest.TestCase):
 
-    def test_lu(self):
-        A = np.array([[0, 3, 4], [4, 6, 10], [22, 1, 7]])
-        lu = LU(A)
-        np.testing.assert_array_almost_equal(lu.P(), np.array([[0., 1., 0.], [1., 0., 0.], [0., 0., 1.]]), decimal=6)
-        np.testing.assert_array_almost_equal(lu.L(), np.array([[1.,0.,0.],[0.,1.,0.],[5.5,-10.66666667,1.]]), decimal=6)
-        np.testing.assert_array_almost_equal(lu.U(), np.array([[4.,6.,10.],[0.,3.,4.],[0.,0., -5.33333333]]), decimal=6)
+    def test_plot_0(self):
+        assert os.path.isfile('plot_0.png')
 
+    def test_plot_1(self):
+        assert os.path.isfile('plot_1.png')
 
-    def test_inverse(self):
         
-        A = np.array([[0, 3, 4], [4, 6, 10], [22, 1, 7]])       
-        lu = LU(A)
-        Ainv = lu.inverse()
-        np.testing.assert_array_almost_equal(Ainv, np.linalg.inv(A), decimal=6)
-
-
-    def test_det(self):
-        
-        A = np.array([[0, 3, 4], [4, 6, 10], [22, 1, 7]])       
-        lu = LU(A)
-        detA = lu.det()
-        np.testing.assert_array_almost_equal(detA, np.linalg.det(A), decimal=6)
-
-#        
 if __name__ == '__main__':
     unittest.main()
